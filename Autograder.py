9541ccdcacac8a0ebb661fefd674cdd95da7fd3e
Manasa Hari <harimanasa17@gmail.com>
2019-11-20 15:32:32 -0800

    Update Autograder.py

--- Autograder.py
+++ Autograder.py
@@ -27,7 +27,7 @@ for (dirname, dirs, files) in os.walk('./sorted_dirs/'):
             #print (string2)
             os.system("cp ./sorted_dirs/"+string0.group(0)+"/"+filename+" ./sorted_dirs/"+string0.group(0)+"/"+string2)
 
-os.system("for d in sorted_dirs/*/; do cp Main.cpp \"$d\"; done")
+os.system("for d in sorted_dirs/*/; do cp main.cc \"$d\"; done")
 os.system("cd sorted_dirs")
 
 COUNT =0
@@ -47,21 +47,21 @@ for (dirname, dirs, files) in os.walk('.'):
        if (filename.endswith('.h') and (("_") not in filename)) :
         increment()
 	#uncomment below to preprend header file in main:w	        
-        insert(dirname+"/Main.cpp", "#include \""+filename+"\" \n")	
+        insert(dirname+"/main.cc", "#include \""+filename+"\" \n")	
 #print(COUNT)
 
 for (dirname, dirs, files) in os.walk('.'):
    for filename in files:
-       if filename.endswith('.cc') :
+       if filename.endswith('.cc') and filename != 'main.cc':
             thefile = os.path.join(dirname,filename)
             test_string = filename            
             object_name = test_string.replace(".cc",".o")
-            os.system("g++ -c "+dirname+"/Main.cpp "+ thefile)
-            os.system("g++ Main.o "+object_name)
+            os.system("g++ -c "+dirname+"/main.cc "+ thefile)
+            os.system("g++ main.o "+object_name)
             os.system("./a.out >> "+dirname+"_output.txt")
-            os.system("echo \" \"; echo \" ====================== \" >> FinalResults.txt ")
-            os.system("echo  Differences between Ideal_output.txt and" +dirname +"_output.txt  >> FinalResults.txt")
-            os.system("diff -w Ideal_output.txt "+dirname+"_output.txt >> FinalResults.txt")
+            os.system("echo \" \"; echo \" ====================== \" >> FinalResults_differences_withIdeal.txt ")
+            os.system("echo  Differences between Ideal_output.txt and" +dirname +"_output.txt  >> FinalResults_differences_withIdeal.txt")
+            os.system("diff -w Ideal_output.txt "+dirname+"_output.txt >> FinalResults_differences_withIdeal.txt")
             os.remove('./a.out')
             print("File a.out removed")
 
@@ -70,4 +70,3 @@ for (dirname, dirs, files) in os.walk('.'):
 
            
 
-           
